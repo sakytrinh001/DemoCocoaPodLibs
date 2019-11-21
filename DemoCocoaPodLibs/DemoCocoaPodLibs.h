c41d13327a04b578fdd7a6e2fd7faf81a3bca9da
Nguyen Minh Thanh <thanh91pro@gmail.com>
2019-11-21 15:46:12 +0700

    Initial Commit

---
+++ DemoCocoaPodLibs/DemoCocoaPodLibs.h
@@ -0,0 +1,19 @@
+//
+//  DemoCocoaPodLibs.h
+//  DemoCocoaPodLibs
+//
+//  Created by administrator on 11/21/19.
+//  Copyright © 2019 Vision. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoCocoaPodLibs.
+FOUNDATION_EXPORT double DemoCocoaPodLibsVersionNumber;
+
+//! Project version string for DemoCocoaPodLibs.
+FOUNDATION_EXPORT const unsigned char DemoCocoaPodLibsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoCocoaPodLibs/PublicHeader.h>
+
+
